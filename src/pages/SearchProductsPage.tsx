8fdd5489d90759857d54b93c81be62531d98068c
sergiolozav <sergiolozav@gmail.com>
2024-01-11 17:12:45 -0400

    feat: SearchProducts busca productos

--- src/pages/SearchProductsPage.tsx
+++ src/pages/SearchProductsPage.tsx
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import { PageHeaderWrapper } from "../components/PageHeaderWrapper";
+import { useSearchProducts } from "../services-hooks/useSearchProducts";
 
 export function SearchProductsPage() {
+  const [search, setSearch] = useState("");
+  const { data, loading, searchProducts } = useSearchProducts(search);
+
+  function handleSearchButton() {
+    searchProducts();
+  }
+  function handleInput(event: React.FormEvent<HTMLInputElement>) {
+    setSearch(event.currentTarget.value);
+  }
+
   return (
-    <PageHeaderWrapper>
-      <div className="mt-16 px-6 flex text-center flex-col w-full items-center justify-center text-white">
-        <h1 className="text-xl md:text-3xl font-bold">
-          Comparador de precios Bolivia
-        </h1>
-        <h2 className="mt-2 text-lg md:text-xl">
-          Busca y compara productos ofrecidos por distintos supermercados
-          bolivianos
-        </h2>
-        <div className="mt-4 flex gap-2">
-          <input
-            className="px-4 py-2 rounded-lg text-lg outline-none text-black"
-            type="text"
-            placeholder="Busca comida, bebidas, dulces..."
-          />
-          <button className="px-2 rounded-lg font-semibold bg-violet-900">
-            Buscar
-          </button>
+    <>
+      <PageHeaderWrapper>
+        <div className="mt-16 px-6 flex text-center flex-col w-full items-center justify-center text-white">
+          <h1 className="text-xl md:text-3xl font-bold">
+            Comparador de precios Bolivia
+          </h1>
+          <h2 className="mt-2 text-lg md:text-xl">
+            Busca y compara productos ofrecidos por distintos supermercados
+            bolivianos
+          </h2>
+          <div className="mt-4 flex gap-2">
+            <input
+              className="px-4 py-2 rounded-lg text-lg outline-none text-black"
+              onChange={handleInput}
+              type="text"
+              placeholder="Busca comida, bebidas, dulces..."
+            />
+            <button
+              className="px-2 rounded-lg font-semibold bg-violet-900"
+              onClick={handleSearchButton}
+            >
+              Buscar
+            </button>
+          </div>
         </div>
-      </div>
-    </PageHeaderWrapper>
+      </PageHeaderWrapper>
+      <div> {loading ? "..." : JSON.stringify(data)}</div>
+    </>
   );
 }
