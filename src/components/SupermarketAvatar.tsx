85bfb3fdc2e6bff48fcd44b5e79934a837045885
sergiolozav <sergiolozav@gmail.com>
2024-01-13 01:25:33 -0400

    feat: Creado avatar para logo de ecommerce

---
+++ src/components/SupermarketAvatar.tsx
@@ -0,0 +1,26 @@
+type SupermarketAvatarType = {
+  marketName: string;
+};
+
+export function SupermarketAvatar(props: SupermarketAvatarType) {
+  return (
+    <div className="flex items-center">
+      <div className="rounded-full bg-stone-200">
+        <img
+          className="h-6 mix-blend-multiply"
+          src={marketNameToImage(props.marketName)}
+        />
+      </div>
+    </div>
+  );
+}
+
+function marketNameToImage(marketName: string) {
+  const mapMarketToImage: { [key: string]: string } = {
+    Farmacorp: "/assets/farmacorp-logo.png",
+    Fidalga: "/assets/fidalga-logo.png",
+    Icnorte: "/assets/icnorte-logo.jpg",
+  };
+
+  return mapMarketToImage[marketName];
+}
