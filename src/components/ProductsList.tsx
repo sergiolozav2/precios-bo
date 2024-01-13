3ce8f5906859e3077966a6415e08dcf78973c26f
sergiolozav <sergiolozav@gmail.com>
2024-01-13 01:26:12 -0400

    feat: Creado componente lista de productos

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,15 @@
+import { ProductType } from "../types/Product";
+import { ProductCard } from "./ProductCard";
+
+type ProductsList = {
+  products: ProductType[];
+};
+export function ProductsList(props: ProductsList) {
+  return (
+    <div className="grid gap-x-2 gap-y-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+      {props.products.map((product) => (
+        <ProductCard key={product.title} product={product} />
+      ))}
+    </div>
+  );
+}
