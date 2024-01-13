1add2f4918cfcafb44de4805296158a369925e91
sergiolozav <sergiolozav@gmail.com>
2024-01-13 03:35:23 -0400

    feat: Busca productos del backend

--- src/services/searchProducts.tsx
+++ src/services/searchProducts.tsx
@@ -1,4 +1,12 @@
-export async function searchProducts(query: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return {data: [query]};
+import { ProductType } from "../types/Product";
+import { backend } from "./api";
+import { ApiReturnType } from "./types";
+
+export async function searchProducts(
+  query: string
+): ApiReturnType<{ items: ProductType[] }> {
+  const response = await backend.get(
+    `search_products/products?search=${query}`
+  );
+  return { data: response.data };
 }
