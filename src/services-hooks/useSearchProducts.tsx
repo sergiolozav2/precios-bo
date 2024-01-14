f23191216fc8447e85e85dd700d97840761e4404
sergiolozav <sergiolozav@gmail.com>
2024-01-14 17:44:09 -0400

    fix: Ya no busca resultados al renderizarse

--- src/services-hooks/useSearchProducts.tsx
+++ src/services-hooks/useSearchProducts.tsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { searchProducts } from "../services/searchProducts";
 import { useQuery } from "@tanstack/react-query";
 
 export function useSearchProducts(initialQuery: string) {
   const [query, setQuery] = useState(initialQuery);
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, error, isPending, refetch } = useQuery({
     queryKey: ["search-products", query],
     queryFn: () => searchProducts(query),
+    enabled: false,
   });
+
+  useEffect(() => {
+    if (query !== "") {
+      refetch();
+    }
+  }, [query, refetch]);
+
   return {
     data: data?.data,
     loading: isLoading,
     error,
+    isPending,
     searchProducts: (newQuery: string) => setQuery(newQuery),
   };
 }
