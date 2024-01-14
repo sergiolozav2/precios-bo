0cd47d6d31082d42ca4b9b6ab0ea3cf58c52e2b6
sergiolozav <sergiolozav@gmail.com>
2024-01-14 15:31:44 -0400

    feat: Creado esqueleto de carga para productos

---
+++ src/components/ProductsListSkeleton.tsx
@@ -0,0 +1,16 @@
+import { ProductCardSkeleton } from "./ProductCardSkeleton";
+
+type ProductsList = {
+  quantity: number;
+};
+export function ProductsListSkeleton(props: ProductsList) {
+  return (
+    <div className="grid gap-x-2 gap-y-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+      {Array(props.quantity)
+        .fill(0)
+        .map((product) => (
+          <ProductCardSkeleton key={product.title} />
+        ))}
+    </div>
+  );
+}
