2170472b08f63017219daadf709b49a6947218d9
sergiolozav <sergiolozav@gmail.com>
2024-01-16 14:54:24 -0400

    test: Tests integracion búsqueda creados

---
+++ cypress/e2e/searchProducts/SearchProductsPage.cy.ts
@@ -0,0 +1,59 @@
+import { resultProductsData } from "./resultProductsData";
+
+const baseUrl = "http://localhost:5173";
+const apiEndpoint = "/api/search_products/*";
+describe("Search feature", () => {
+  const searchProduct = "coca cola";
+
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: apiEndpoint,
+      },
+      resultProductsData
+    );
+    cy.visit(baseUrl);
+  });
+  it("searches results", () => {
+    cy.get("form").get("input").type(searchProduct);
+    cy.get("form").get("button").click();
+    cy.contains(`Resultados de '${searchProduct}'`).should("exist");
+  });
+
+  it("shows products", () => {
+    cy.get("form").get("input").type(searchProduct);
+    cy.get("form").get("button").click();
+    cy.contains(resultProductsData.data.items[0].title).should("exist");
+    cy.contains(resultProductsData.data.items[1].title).should("exist");
+    cy.contains(resultProductsData.data.items[2].title).should("exist");
+  });
+
+  it("sets url param 'search' on search", () => {
+    const urlParam = new URLSearchParams({ search: searchProduct });
+    cy.get("form").get("input").type(searchProduct);
+    cy.get("form").get("button").click();
+    cy.location("search").should("include", `?${urlParam.toString()}`);
+  });
+});
+
+describe("Load with url param", () => {
+  const searchProduct = "coca cola";
+  const urlParam = new URLSearchParams({ search: searchProduct });
+
+  beforeEach(() => {
+    const baseUrlWithParams = `${baseUrl}?${urlParam.toString()}`;
+    cy.visit(baseUrlWithParams);
+  });
+
+  it("input takes value from url param", () => {
+    cy.get("form").get("input").should("have.value", searchProduct);
+  });
+
+  it("search takes value from url param", () => {
+    cy.intercept(apiEndpoint, (req) => {
+      expect(req.query["search"]).to.equal(searchProduct);
+      req.reply(resultProductsData)
+    });
+  });
+});
