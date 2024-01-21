bcf9f71cc44d9a63ef7ae8745ce0d78b907987c6
sergiolozav <sergiolozav@gmail.com>
2024-01-21 17:40:27 -0400

    fix: Corregido warning for ruta de asset

--- src/components/SvgIcon/SvgIcon.tsx
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -6,7 +6,7 @@ export function SvgIcon(props: SvgIconType) {
   return (
     <img
       className={props.className}
-      src={`./public/assets/svg/${props.iconName}.svg`}
+      src={`./assets/svg/${props.iconName}.svg`}
     />
   );
 }
