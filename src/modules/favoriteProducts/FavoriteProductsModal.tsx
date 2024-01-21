d13e048634cf18d15589b3612a15de1fe0026cf3
sergiolozav <sergiolozav@gmail.com>
2024-01-21 18:43:14 -0400

    feat: Mejora en estilo y espacios

--- src/modules/favoriteProducts/FavoriteProductsModal.tsx
+++ src/modules/favoriteProducts/FavoriteProductsModal.tsx
@@ -5,16 +5,16 @@ export const FavoritesModalURLName = "favoritesModal";
 export function FavoriteProductsModal() {
   const { toggleModal } = useModal(FavoritesModalURLName);
   return (
-    <div className="inset-0 top-auto rounded-t-md sm:right-12 sm:bottom-3 fixed bg-stone-50">
+    <div className="inset-0 top-auto rounded-t-md sm:left-auto sm:right-12 sm:bottom-3 fixed bg-stone-50">
       <div className="min-w-64 relative flex flex-col">
-        <button className="absolute top-2.5 right-2" onClick={toggleModal}>
+        <button className="absolute p-0.5 top-3 right-1.5 rounded-full hover:bg-stone-300" onClick={toggleModal}>
           <SvgIcon className="h-5" iconName={Icons.IoMdClose} />
         </button>
-        <div className="h-full py-2 min-h-32 flex flex-col text-center">
+        <div className="py-3 min-h-32 h-full flex flex-col text-center">
           <h3 className="px-4 text-sm font-semibold"> Productos favoritos </h3>
           <div className="py-4 flex-1 flex items-center justify-center">
             <p className="px-4 text-sm text-stone-800">
-              No tienes productos favoritos guardados
+              No tienes productos favoritos guardados.
             </p>
           </div>
         </div>
