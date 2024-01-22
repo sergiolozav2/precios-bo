1c77287c299e338c535455bc6a4dcfc6f07b95b6
sergiolozav <sergiolozav@gmail.com>
2024-01-22 11:48:02 -0400

    feat: Muestra productos favoritos

---
+++ src/modules/favoriteProducts/FavoriteProductsContent.tsx
@@ -0,0 +1,30 @@
+import { productsEqual } from "../../types/Product";
+import { ProductCard } from "../searchProducts/components/ProductCard";
+import { useFavoriteProducts } from "./providers/FavoriteProductsProvider";
+
+export function FavoriteProductsContent() {
+  const { products, saveOrDeleteProduct } = useFavoriteProducts();
+  const hasProducts = !(products.length === 0);
+
+  if (!hasProducts) {
+    return (
+      <div className="flex-1 grid place-items-center">
+        <p className="px-4 text-sm text-stone-800">
+          No tienes productos favoritos guardados.
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div className="py-4 flex-1 max-h-[36rem] gap-2 grid grid-cols-2 text-left overflow-auto">
+      {products.map((product) => (
+        <ProductCard
+          key={product.title}
+          product={product}
+          onFavoriteClick={() => saveOrDeleteProduct(product)}
+          isFavorite={!!products.find((p) => productsEqual(p, product))}
+        />
+      ))}
+    </div>
+  );
+}
--- src/modules/favoriteProducts/FavoriteProductsDialog.tsx
+++ src/modules/favoriteProducts/FavoriteProductsDialog.tsx
@@ -1,22 +1,23 @@
 import { SvgIcon } from "../../components/SvgIcon";
+import { FavoriteProductsContent } from "./FavoriteProductsContent";
 import { useModal } from "./hooks/useModal";
 
 export const FavoritesDialogURLName = "favoritesModal";
 export function FavoriteProductsDialog() {
   const { toggleModal } = useModal(FavoritesDialogURLName);
+
   return (
-    <div className="inset-0 top-auto rounded-t-md sm:left-auto sm:right-12 sm:bottom-3 fixed bg-stone-50">
+    <div className="inset-0 top-auto rounded-t-md sm:w-[32rem] sm:left-auto sm:right-12 sm:bottom-3 fixed bg-stone-50">
       <div className="min-w-64 relative flex flex-col">
-        <button className="absolute p-0.5 top-3 right-1.5 rounded-full hover:bg-stone-300" onClick={toggleModal}>
+        <button
+          className="absolute p-0.5 top-3 right-1.5 rounded-full hover:bg-stone-300"
+          onClick={toggleModal}
+        >
           <SvgIcon className="h-5" iconName="IoMdClose" />
         </button>
         <div className="py-3 min-h-32 h-full flex flex-col text-center">
           <h3 className="px-4 text-sm font-semibold"> Productos favoritos </h3>
-          <div className="py-4 flex-1 flex items-center justify-center">
-            <p className="px-4 text-sm text-stone-800">
-              No tienes productos favoritos guardados.
-            </p>
-          </div>
+          <FavoriteProductsContent />
         </div>
         <button
           className="px-4 py-2 sm:rounded-b-md font-medium bg-violet-900 text-stone-50"
