6564d3e0bebac7a5a07259922cfac6eaa5e23983
sergiolozav <sergiolozav@gmail.com>
2024-01-22 02:10:38 -0400

    feat: Hook para obtener productos favoritos

---
+++ src/modules/hooks/useFavoriteProductsStorage.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { ProductType, productsEqual } from "../../types/Product";
+
+const productsKey = "products";
+export function useFavoriteProductsStorage() {
+  const [products, setProducts] = useState(() =>
+    getFavoritesFromLocalStorage()
+  );
+
+  function saveProduct(product: ProductType) {
+    const index = products.findIndex((p) => productsEqual(p, product));
+
+    let newProducts: ProductType[] = [];
+
+    if (index === -1) {
+      newProducts = [product, ...products];
+    } else {
+      products.splice(index, 1);
+      newProducts = products;
+    }
+
+    setProducts([...newProducts]);
+    localStorage.setItem(productsKey, JSON.stringify(newProducts));
+  }
+
+  function getFavoritesFromLocalStorage() {
+    const favoritesString = localStorage.getItem(productsKey);
+    let favorites: ProductType[] = [];
+    if (favoritesString) {
+      favorites = JSON.parse(favoritesString);
+    }
+    return favorites;
+  }
+  return { products, saveProduct };
+}
