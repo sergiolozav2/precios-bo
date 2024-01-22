bafa2db4584fa8702021314027af956c4b18c093
sergiolozav <sergiolozav@gmail.com>
2024-01-22 02:11:23 -0400

    feat: Funcion para comprar igualdad productos

--- src/types/Product.tsx
+++ src/types/Product.tsx
@@ -5,3 +5,15 @@ export type ProductType = {
   source: string;
   price: number;
 };
+
+/**
+ * I don't use ProductType.image because it changes sometimes
+ */
+export function productsEqual(a: ProductType, b: ProductType) {
+  return (
+    a.title === b.title &&
+    a.price === b.price &&
+    a.source === b.source &&
+    a.link === b.link
+  );
+}
