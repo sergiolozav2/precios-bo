cd497a64d6879270cb69f7d63ffab28c98c61384
sergiolozav <sergiolozav@gmail.com>
2024-01-22 02:20:04 -0400

    feat: Lista ordena productos

--- src/modules/searchProducts/components/ProductsList.tsx
+++ src/modules/searchProducts/components/ProductsList.tsx
@@ -1,14 +1,31 @@
 import { ProductType } from "../../../types/Product";
+import { useFavoriteProducts } from "../../favoriteProducts/providers/FavoriteProductsProvider";
 import { ProductCard } from "./ProductCard";
 
 type ProductsList = {
   products: ProductType[];
 };
 export function ProductsList(props: ProductsList) {
+  const query = "leche";
+  const sortedProducts = props.products.sort((a, b) => {
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
+    return (
+      titleA.indexOf(query) * 2 + a.price - titleB.indexOf(query) * 2 - b.price
+    );
+  });
+
+  const { saveOrDeleteProduct, products } = useFavoriteProducts();
   return (
     <div className="grid gap-x-2 gap-y-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-      {props.products.map((product) => (
-        <ProductCard key={product.title} product={product} />
+      {sortedProducts.map((product) => (
+        <ProductCard
+          key={product.title}
+          product={product}
+          imageFallback="./assets/fallback-image.png"
+          onFavoriteClick={() => saveOrDeleteProduct(product)}
+          isFavorite={!!products.find((p) => p.title === product.title)}
+        />
       ))}
     </div>
   );
