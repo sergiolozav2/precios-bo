69ead25fc8c651c693942a1496e0d868140f1f08
sergiolozav <sergiolozav@gmail.com>
2024-01-30 14:56:52 -0400

    feat: Icono nuevo MdSearchOff

--- src/components/SvgIcon/SvgAllIconPaths.tsx
+++ src/components/SvgIcon/SvgAllIconPaths.tsx
@@ -5,6 +5,7 @@ import { IoMdClose } from "./IconsPaths/IoMdClose";
 import { MdKeyboardDoubleArrowUp } from "./IconsPaths/MdKeyboardDoubleArrowUp";
 import { MdOutlineMoreTime } from "./IconsPaths/MdOutlineMoreTime";
 import { MdQueryBuilder } from "./IconsPaths/MdQueryBuilder";
+import { MdSearchOff } from "./IconsPaths/MdSearchOff";
 
 export const Icons = {
   FaMoneyCheckDollar: { path: <FaMoneyCheckDollar />, viewBox: "0 0 576 512" },
@@ -14,4 +15,5 @@ export const Icons = {
   MdKeyboardDoubleArrowUp: { path: <MdKeyboardDoubleArrowUp />, viewBox: "0 0 24 24" },
   MdQueryBuilder: { path: <MdQueryBuilder />, viewBox: "0 0 24 24" },
   IoMdClose: { path: <IoMdClose />, viewBox: "0 0 512 512" },
+  MdSearchOff: { path: <MdSearchOff />, viewBox: "0 0 24 24" },
 };
