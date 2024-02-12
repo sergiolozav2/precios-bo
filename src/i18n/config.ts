413a7124cc4a27ccf41f35af0d3e9837e00eb1ef
sergiolozav <sergiolozav@gmail.com>
2024-02-12 13:47:49 -0400

    feat: Config para i18n

---
+++ src/i18n/config.ts
@@ -0,0 +1,12 @@
+import i18n from "i18next";
+import HttpBackend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(HttpBackend)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+    debug: true,
+  });
