ae6b4c341d1053b7fcea15233c9c1ad19c10e0ac
sergiolozav <sergiolozav@gmail.com>
2024-02-13 12:58:35 -0400

    fix: Evita layout shift al abrir radix select

--- src/modules/favoriteProducts/FloatingButton.tsx
+++ src/modules/favoriteProducts/FloatingButton.tsx
@@ -19,29 +19,32 @@ export function FloatingButton(props: FloatingButtonType) {
   const { t } = useTranslation();
 
   return (
-    <div className="fixed right-12 bottom-3">
-      <Dialog open={show}>
-        <DialogTrigger asChild>
-          <button
-            className="rounded-full aspect-square sm:aspect-auto sm:rounded-md font-medium text-stone-50 bg-violet-900"
-            onClick={toggleModal}
-          >
-            <div className="px-4 py-2 min-w-56 hidden sm:flex justify-between">
-              <span> {t("favorites.button.title")} </span>
-              <SvgIcon className="h-5" iconName="MdKeyboardDoubleArrowUp" />
-            </div>
-            <div className="p-3 sm:hidden">
-              <SvgIcon className="h-5" iconName="FaHeart" />
-            </div>
-          </button>
-        </DialogTrigger>
-        <DialogPortal>
-          <DialogOverlay className="inset-0 fixed bg-stone-900/15" />{" "}
-          <DialogContent onEscapeKeyDown={toggleModal}>
-            {props.children}
-          </DialogContent>
-        </DialogPortal>
-      </Dialog>
+    <div className="bottom-0 w-screen fixed">
+      {/* Evita layout shift cuando scrollbar desaparece por Radix. */}
+      <div className="absolute right-12 bottom-3">
+        <Dialog open={show}>
+          <DialogTrigger asChild>
+            <button
+              className="rounded-full aspect-square sm:aspect-auto sm:rounded-md font-medium text-stone-50 bg-violet-900"
+              onClick={toggleModal}
+            >
+              <div className="px-4 py-2 min-w-56 hidden sm:flex justify-between">
+                <span> {t("favorites.button.title")} </span>
+                <SvgIcon className="h-5" iconName="MdKeyboardDoubleArrowUp" />
+              </div>
+              <div className="p-3 sm:hidden">
+                <SvgIcon className="h-5" iconName="FaHeart" />
+              </div>
+            </button>
+          </DialogTrigger>
+          <DialogPortal>
+            <DialogOverlay className="inset-0 fixed bg-stone-900/15" />{" "}
+            <DialogContent onEscapeKeyDown={toggleModal}>
+              {props.children}
+            </DialogContent>
+          </DialogPortal>
+        </Dialog>
+      </div>
     </div>
   );
 }
