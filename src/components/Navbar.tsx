e3852d2e849a79914bbe69a27188c0673ae06f38
sergiolozav <sergiolozav@gmail.com>
2024-02-13 12:49:31 -0400

    feat: Agregado LanguageSwitcher a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from "react-i18next";
 import { routesNames } from "../routes/routesNames";
 import { NavbarLink } from "./NavbarLink";
+import { LanguageSwitcher } from "./LanguageSwitcher";
 
 export function Navbar() {
   const { t } = useTranslation();
+
   return (
     <nav className="p-4 absolute top-0 flex justify-between items-center w-full bg-transparent text-white">
       <div className="flex items-center">
@@ -13,11 +15,12 @@ export function Navbar() {
         <span className="ml-4 tracking-wide font-bold text-xl">PreciosBo</span>
       </div>
 
-      <div className="gap-6 hidden md:flex">
+      <div className="gap-6 hidden md:flex items-center">
         <NavbarLink label={t("navbar.start")} to={routesNames.Inicio} />
         <NavbarLink label={t("navbar.blog")} to={routesNames.Funcion} />
         <NavbarLink label={t("navbar.documentation")} to="/" />
         <NavbarLink label={t("navbar.contact")} to={routesNames.Contacto} />
+        <LanguageSwitcher />
       </div>
     </nav>
   );
