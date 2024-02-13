f9594ea3b427e96006f873ef06664d69c8e7820e
sergiolozav <sergiolozav@gmail.com>
2024-02-13 12:59:49 -0400

    feat: Desactiva retries de ReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,13 @@ import { router } from "./routes/routes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n/config.ts";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
