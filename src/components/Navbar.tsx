c8cbc88f8ff7090c32de9304f18b5111dcd05943
Sergio Loza <sergiolozav@gmail.com>
2024-03-07 01:01:40 -0400

    fix: Query params se mantienen al ir a Inicio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import { useTranslation } from "react-i18next";
 import { routesNames } from "../routes/routesNames";
 import { NavbarLink } from "./NavbarLink";
 import { LanguageSwitcher } from "./LanguageSwitcher";
+import { useLocation } from "react-router-dom";
 
 export function Navbar() {
   const { t } = useTranslation();
-
+  const { search } = useLocation();
   return (
     <nav className="p-4 absolute top-0 flex justify-between items-center w-full bg-transparent text-white">
       <div className="flex items-center">
@@ -16,7 +17,10 @@ export function Navbar() {
       </div>
 
       <div className="gap-6 hidden md:flex items-center">
-        <NavbarLink label={t("navbar.start")} to={routesNames.Inicio} />
+        <NavbarLink
+          label={t("navbar.start")}
+          to={routesNames.Inicio + search}
+        />
         <NavbarLink label={t("navbar.blog")} to={routesNames.Funcion} />
         <NavbarLink label={t("navbar.documentation")} to="/" />
         <NavbarLink label={t("navbar.contact")} to={routesNames.Contacto} />
